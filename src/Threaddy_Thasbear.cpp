0b1ce4fed8871be586d9d9e63097ca7b53c2a5fa
jackson_0987979 <0987979@my.scccd.edu>
2024-04-28 22:06:11 -0700

    Barebones

--- src/Threaddy_Thasbear.cpp
+++ src/Threaddy_Thasbear.cpp
@@ -1,9 +1,23 @@
 #include "../lib/read.h"
-#include <vector>
-#include <unordered_map>
 #include <thread>
 using namespace std;
 
+/*
+Need functions for:
+Enemies, thier AI will update on it's own thread
+Players, they will have their own thread
+Physics of projectiles, this one will probably have 2 threads, whenever there are more than 30 projectiles it'll put the rest on the second thread
+*/
+
+void enemy_threading() {
+}
+
+void player_threading() { 
+}
+
+void projectile_threading() {
+}
+
 int main() {
-	cout << "Maximum effort\n";
+
 }
