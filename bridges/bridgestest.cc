3bd4f9481954213ecc4aba8817ab314fe5bfb3d0
silverio_0944672 <0944672@my.scccd.edu>
2024-04-30 15:20:51 -0700

    Bridges test

--- bridges/bridgestest.cc
+++ bridges/bridgestest.cc
@@ -5,6 +5,25 @@ using namespace std;
 using namespace bridges;
 
 int main() {
-	Bridges::initialize(5, "gugu", "1058853664803");
-	//AudioClip clip(...);
+	//TODO: Make one SHORT SONG using BRIDGES (I'm only making one) to meet role reqs
+	//TODO: Make SFX using BRIDGES to meet role reqs (This one is doable)
+	Bridges bridges(5, "gugu", "1058853664803");
+	bridges.setTitle("Task Force Smth OST");
+	AudioClip song("main_theme.wav"); //In the actual game, we'll probably be changing the song every time an event occurs
+
+	int sampleCount = song.getSampleCount();
+	int channelCount = song.getNumChannels();
+	int bitDepth = song.getSampleBits();
+	int sampleRate = song.getSampleRate();
+
+	cout << "Sample Count: " << sampleCount << "\n"
+		 << "Channel Count: " << channelCount << "\n"
+		 << "Bit Depth: " << bitDepth << "\n"
+		 << "Sample Rate: " << sampleRate << "\n";
+
+
+	bridges.setDataStructure(&song);
+	bridges.visualize();
+
+	return 0;
 }
