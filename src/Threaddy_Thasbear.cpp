be231238af1ac3422ceb2806b426273d296b68f1
jackson_0987979 <0987979@my.scccd.edu>
2024-05-03 14:35:26 -0700

    Bro thats it ;___;

--- src/Threaddy_Thasbear.cpp
+++ src/Threaddy_Thasbear.cpp
@@ -2,38 +2,17 @@
 #include <thread>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-/*
-Need functions for:
-Enemies, thier AI will update on it's own thread
-Players, they will have their own thread
-Physics of projectiles, this one will probably have 2 threads, whenever there are more than 30 projectiles it'll put the rest on the second thread
-Input Thread
-Output Thread
-*/
+// TODO: Thread manager class
 
-void enemy_threading() {
-}
+class ThreadManager {
+public:
+	unordered_map<string, jthread> map;
 
-void player_threading() { 
-}
+	void new_thread(string s, auto function) {
+		map.insert({s, function});
+	}
 
-void projectile_threading() {
-}
-
-void input_thread() {
-}
-
-void output_thread() {
-}
-
-int main() {
-	vector<jthread> vec;
-	// Some code doing actual threading
-
-	// Joining all threads
-	for (int i = 0; i < vec.size(); i++) {vec.at(i).join();}
-
-
-}
+};
